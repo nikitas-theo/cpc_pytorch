7dc4e257adc436e0484e257a03b0bd8a651d6b4e
nikitas-theo <nikitastheodorop@gmail.com>
2026-06-01 11:41:30 +0200

    init comit

--- conf_train_cpc_model_orig_implementation.py
+++ conf_train_cpc_model_orig_implementation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Author: Einari Vaaras, einari.vaaras@tuni.fi, Tampere University
 Speech and Cognition Research Group, https://webpages.tuni.fi/specog/index.html
@@ -36,68 +35,63 @@ load_model = 0
 save_best_model = 1
 
 # The name of the text file into which we log the output of the training process
-name_of_log_textfile = 'cpc_trainlog_orig_implementation.txt'
+name_of_log_textfile = "cpc_trainlog_orig_implementation.txt"
 
 # A flag for determining whether we want to print the contents of the configuration file into the
 # logging file
 print_conf_contents = 1
 
 # Define our models that we want to use from the file cpc_model.py
-encoder_name = 'CPC_encoder'
-autoregressive_model_name = 'CPC_autoregressive_model'
-postnet_name = 'CPC_postnet'
+encoder_name = "CPC_encoder"
+autoregressive_model_name = "CPC_autoregressive_model"
+postnet_name = "CPC_postnet"
 
 # A flag for defining if we are using RNNs in our AR model (i.e., do we need a hidden vector)
 rnn_models_used_in_ar_model = 1
 
 # Define our dataset for our data loader that we want to use from the file cpc_data_loader.py
-dataset_name = 'CPC_raw_audio_dataset'
+dataset_name = "CPC_raw_audio_dataset"
 
 # Define our loss function that we want to use from the file cpc_loss.py
-loss_name = 'CPC_loss_no_classes'
+loss_name = "CPC_loss_no_classes"
 
 # The hyperparameters for the loss function
-loss_params = {'future_predicted_timesteps': future_predicted_timesteps}
+loss_params = {"future_predicted_timesteps": future_predicted_timesteps}
 
 # Define the optimization algorithm we want to use from torch.optim
-optimization_algorithm = 'Adam'
+optimization_algorithm = "Adam"
 
 # The hyperparameters for our optimization algorithm
-optimization_algorithm_params = {'lr': learning_rate}
+optimization_algorithm_params = {"lr": learning_rate}
 
 # A flag to determine if we want to use a learning rate scheduler
 use_lr_scheduler = 1
 
 # Define which learning rate scheduler we want to use from torch.optim.lr_scheduler
-lr_scheduler = 'ReduceLROnPlateau'
+lr_scheduler = "ReduceLROnPlateau"
 
 # The hyperparameters for the learning rate scheduler
-lr_scheduler_params = {'mode': 'min',
-                       'factor': 0.5,
-                       'patience': 30}
+lr_scheduler_params = {"mode": "min", "factor": 0.5, "patience": 30}
 
 # The names of the best models (according to validation loss) for loading/saving model weights
-encoder_best_model_name = 'CPC_Encoder_best_model.pt'
-ar_best_model_name = 'CPC_AR_best_model.pt'
-w_best_model_name = 'W_best_model.pt'
+encoder_best_model_name = "CPC_Encoder_best_model.pt"
+ar_best_model_name = "CPC_AR_best_model.pt"
+w_best_model_name = "W_best_model.pt"
 
 # The hyperparameters for constructing the models. An empty dictionary will make the model to use
 # only default hyperparameters, i.e., the hyperparameters of the original CPC paper
-encoder_params = {'dropout': dropout}
+encoder_params = {"dropout": dropout}
 ar_model_params = {}
 w_params = {}
 
 # The hyperparameters for our data loaders
-params_train_dataset = {}
-params_validation_dataset = {}
-params_test_dataset = {}
+params_train_dataset = {"file_dir": "LibriSpeech/train-clean-100"}
+params_validation_dataset = {"file_dir": "LibriSpeech/train-clean-100"}
+params_test_dataset = {"file_dir": "LibriSpeech/train-clean-100"}
+
 
 # The hyperparameters for training and validation (arguments for torch.utils.data.DataLoader object)
-params_train = {'batch_size': batch_size,
-                'shuffle': True,
-                'drop_last': True}
+params_train = {"batch_size": batch_size, "shuffle": True, "drop_last": True}
 
 # The hyperparameters for testing (arguments for torch.utils.data.DataLoader object)
-params_test = {'batch_size': batch_size,
-               'shuffle': False,
-               'drop_last': True}
+params_test = {"batch_size": batch_size, "shuffle": False, "drop_last": True}
--- cpc_loss.py
+++ cpc_loss.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Author: Einari Vaaras, einari.vaaras@tuni.fi, Tampere University
 Speech and Cognition Research Group, https://webpages.tuni.fi/specog/index.html
@@ -8,10 +7,10 @@ The loss function for the CPC model.
 """
 
 import sys
-import numpy as np
-from torch import matmul, diag
-from torch.nn import Module, LogSoftmax
 
+import numpy as np
+from torch import diag, matmul
+from torch.nn import LogSoftmax, Module
 
 
 class CPC_loss_no_classes(Module):
@@ -20,58 +19,67 @@ class CPC_loss_no_classes(Module):
     act as the negative samples. Note that in the loss calculation, a log density ratio log(f_k)
     is used as log(f_k) = matmul(Z_future_timesteps[k-1], predicted_future_Z[i].transpose(0,1)),
     w.r.t. Eq. (3) in the original CPC paper where f_k = exp(z_{t+k}^T * W_k * c_t).
-    
+
     _____________________________________________________________________________________________
     Input parameters:
-    
+
     future_predicted_timesteps: The future predicted timesteps (integer or a list of integers)
-        
+
     Z_future_timesteps: The encodings of the future timesteps, i.e. z_{t+k} where k in
                         [1, 2, ..., num_future_predicted_timesteps].
-    
+
     predicted_future_Z: The predicted future embeddings z_{t+k} where k in
                         [1, 2, ..., num_future_predicted_timesteps]
-    
+
     _____________________________________________________________________________________________
-    
+
     """
-    
+
     def __init__(self, future_predicted_timesteps=12):
         super().__init__()
-        
+
         # We first determine whether our future_predicted_timesteps is a number or a list of numbers.
         if isinstance(future_predicted_timesteps, int):
             # future_predicted_timesteps is a number, so we have future_predicted_timesteps loss calculations
-            self.future_predicted_timesteps = np.arange(1, future_predicted_timesteps + 1)
-            
+            self.future_predicted_timesteps = np.arange(
+                1, future_predicted_timesteps + 1
+            )
+
         elif isinstance(future_predicted_timesteps, list):
             # future_predicted_timesteps is a list of numbers, so we have len(future_predicted_timesteps) loss calculations
             self.future_predicted_timesteps = future_predicted_timesteps
-            
+
         else:
-            sys.exit('Configuration setting "future_predicted_timesteps" must be either an integer or a list of integers!')
+            sys.exit(
+                'Configuration setting "future_predicted_timesteps" must be either an integer or a list of integers!'
+            )
 
     def forward(self, Z_future_timesteps, predicted_future_Z):
-        
+
         log_smax = LogSoftmax(dim=1)
-        
+
         loss = 0
         num_future_predicted_timesteps = len(self.future_predicted_timesteps)
         batch_size = Z_future_timesteps.size()[1]
-        
+
         # We go through each future timestep and compute the loss. Z_future_timesteps is of size
         # [future_predicted_timesteps, batch_size, num_features] or [len(future_predicted_timesteps), batch_size, num_features]
         # where num_features is the size of the encoding for each timestep produced by the encoder
         # --> with default values Z_future_timesteps.size() = torch.Size([12, 8, 512])
         i = 0
         for k in self.future_predicted_timesteps:
-            loss -= diag(log_smax(matmul(Z_future_timesteps[k-1], predicted_future_Z[i].transpose(0,1)))).sum(dim=0)
+            loss -= diag(
+                log_smax(
+                    matmul(
+                        Z_future_timesteps[k - 1], predicted_future_Z[i].transpose(0, 1)
+                    )
+                )
+            ).sum(dim=0)
             i += 1
-            
+
         loss = loss / (batch_size * num_future_predicted_timesteps)
-        
+
         return loss
-    
 
 
 class CPC_loss(Module):
@@ -83,55 +91,66 @@ class CPC_loss(Module):
     the loss calculation, a log density ratio log(f_k) is used as
     log(f_k) = matmul(Z_future_timesteps[k-1], predicted_future_Z[i].transpose(0,1)),
     w.r.t. Eq. (3) in the original CPC paper where f_k = exp(z_{t+k}^T * W_k * c_t).
-    
+
     _____________________________________________________________________________________________
     Input parameters:
-    
+
     future_predicted_timesteps: The future predicted timesteps (integer or a list of integers)
-        
+
     Z_future_timesteps: The encodings of the future timesteps, i.e. z_{t+k} where k in
                         [1, 2, ..., num_future_predicted_timesteps].
-    
+
     predicted_future_Z: The predicted future embeddings z_{t+k} where k in
                         [1, 2, ..., num_future_predicted_timesteps]
-                        
+
     batch_labels: The labels of the elements in each batch
-    
+
     _____________________________________________________________________________________________
-    
+
     """
-    
+
     def __init__(self, future_predicted_timesteps=12):
         super().__init__()
-        
+
         # We first determine whether our future_predicted_timesteps is a number or a list of numbers.
         if isinstance(future_predicted_timesteps, int):
             # future_predicted_timesteps is a number, so we have future_predicted_timesteps loss calculations
-            self.future_predicted_timesteps = np.arange(1, future_predicted_timesteps + 1)
-            
+            self.future_predicted_timesteps = np.arange(
+                1, future_predicted_timesteps + 1
+            )
+
         elif isinstance(future_predicted_timesteps, list):
             # future_predicted_timesteps is a list of numbers, so we have len(future_predicted_timesteps) loss calculations
             self.future_predicted_timesteps = future_predicted_timesteps
-            
+
         else:
-            sys.exit('Configuration setting "future_predicted_timesteps" must be either an integer or a list of integers!')
+            sys.exit(
+                'Configuration setting "future_predicted_timesteps" must be either an integer or a list of integers!'
+            )
 
     def forward(self, Z_future_timesteps, predicted_future_Z, batch_labels):
-        
+
         log_smax = LogSoftmax(dim=1)
-        
+
         loss = 0
         num_future_predicted_timesteps = len(self.future_predicted_timesteps)
         batch_labels_unique = list(dict.fromkeys(batch_labels))
         indices_with_unique_label = [batch_labels.index(x) for x in batch_labels_unique]
-            
+
         i = 0
         for k in self.future_predicted_timesteps:
-            loss -= diag(log_smax(matmul(Z_future_timesteps[k-1, indices_with_unique_label], predicted_future_Z[i, indices_with_unique_label].transpose(0,1)))).sum(dim=0)
+            loss -= diag(
+                log_smax(
+                    matmul(
+                        Z_future_timesteps[k - 1, indices_with_unique_label],
+                        predicted_future_Z[i, indices_with_unique_label].transpose(
+                            0, 1
+                        ),
+                    )
+                )
+            ).sum(dim=0)
             i += 1
-                
-        loss = loss / (len(indices_with_unique_label) * num_future_predicted_timesteps)
-        
-        
-        return loss
 
+        loss = loss / (len(indices_with_unique_label) * num_future_predicted_timesteps)
+
+        return loss
